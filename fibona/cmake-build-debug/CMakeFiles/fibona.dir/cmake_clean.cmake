eaf87eadecc65c2fcf3a8ef16f94b457d2973362
invictusmaneo63 <ptiwari2407@gmail.com>
2019-11-21 15:46:29 +0100

    Git fibinacci

---
+++ fibona/cmake-build-debug/CMakeFiles/fibona.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fibona.dir/main.cpp.obj"
+  "fibona.exe"
+  "fibona.exe.manifest"
+  "fibona.pdb"
+  "libfibona.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fibona.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ fibona/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#define MAX 100
+using  namespace std;
+int fib(int n){
+    cout << "fi " << n << endl;
+    int f[n+2];
+//    for (int i = 0 ; i<n+2;i++){
+//        f[i] = i;
+//    }
+//    for (auto i : f){
+//        cout<< '\t' << i;
+//    }
+//    if(n <2){
+f[0] = 0;
+f[1] = 1;
+        for(int i = 2; i<n+2;i++){
+            f[i] = f[i-1]+f[i-2];
+        }
+    return f[n];
+    }
+int main() {
+//    std::cout << "Hello, World!" << std::endl;
+    int result = 0;
+    int n = 40;
+//    cin >> n ;
+    result = fib(n);
+    cout <<"\n" << result;
+
+
+return 0;
+}
